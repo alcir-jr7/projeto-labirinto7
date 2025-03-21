ae10ba046717cf3c10d9340bae1052e1cc255a91
Alcir <ajsj3@discente.ifpe.edu.br>
2025-03-20 22:45:07 -0300

    atualização do menu

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -5,8 +5,9 @@
   
   <h1 class="">Como Jogar Escape the Maze</h1>
   <div id="menu-game" class="quadro-ia">
-    <p>O jogo possui um total de 5 fases que são geradas aleatóriamente, e cada fase tem um tempo determinado para você escapar. Encontre a saída antes que o tempo acabe. Boa sorte!</p>
+    <p>O jogo possui um total de 5 fases que são geradas aleatóriamente, e cada fase tem um tempo determinado para você escapar. Encontre a saída antes que o tempo acabe.</p>
     <p>Use ⬅️⬆️⬇️➡️ no teclado para mover o personagem pelo labirinto.</p>
+    <p>Boa sorte!</p>
   </div>
   <br>
   <br>
